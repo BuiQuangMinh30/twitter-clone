1f3eb5abf86597156a5c3673df985a991651cab8
BuiQuangMinh30 <buiquangminh30102001@gmail.com>
2022-10-10 22:23:09 +0700

    fix title

--- pages/[id].js
+++ pages/[id].js
@@ -54,7 +54,7 @@ import {
       <div>
         <Head>
           <title>
-            {post?.username} on Twitter: "{post?.text}"
+            {post?.username} on Twitter: `{post?.text}`
           </title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
